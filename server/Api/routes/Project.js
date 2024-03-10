53c8171ac5a687d5eac992a787a1eae807e1e749
Palak Gupta <120472704+Palakgupta2002@users.noreply.github.com>
2024-03-10 17:48:03 +0530

    Project details

--- server/Api/routes/Project.js
+++ server/Api/routes/Project.js
@@ -40,5 +40,20 @@ router.get('/getProject/:Musername', async (req, res) => {
         return res.status(500).json({ message: 'Internal server error' });
     }
 });
+router.get('/getProjectDetails/:id',async(req,res)=>{
+    const {id}=req.params;
+    try{
+        const projectDetails=await  Project.find({_id:id});
+        if(!projectDetails){
+            return res.status(404).json({message:'No details found of this project'})
+        }
+        res.json({message:'Project details get succesfully',projectDetails})
+
+    }catch(error){
+        console.log("some errors occurs in this project",error)
+        return res.status(500).json({message:"Internal server error"})
+
+    }
+})
 
 export default router
